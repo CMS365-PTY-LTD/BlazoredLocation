baf720fd8f3c957b7ed10206cbfa69bafd3967d4
Shafaqat Ali <shafaqat.ali@cms365.com.au>
2025-03-26 17:09:09 +1000

    exception handled.

--- BlazoredLocation/Services/DeviceLocation.cs
+++ BlazoredLocation/Services/DeviceLocation.cs
@@ -2,27 +2,37 @@
 using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Devices.Sensors;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
-using Geolocation = BlazoredLocation.Entities.Geolocation;
+using BlazoredGeolocation = BlazoredLocation.Entities.Geolocation;
+using MauiGeolocation = Microsoft.Maui.Devices.Sensors.Geolocation;
 
 namespace BlazoredLocation.Services
 {
     public class DeviceLocation : IDeviceLocation
     {
-        public async Task<Geolocation> GetDeviceLocation(bool getCachedLocationIfAvailable)
+        private CancellationTokenSource _cancelTokenSource;
+        private bool _isCheckingLocation;
+        public async Task<BlazoredGeolocation> GetDeviceLocation(bool getCachedLocationIfAvailable)
         {
             if (getCachedLocationIfAvailable)
             {
                 return await GetCachedLocation();
             }
-            return null;
+            return await GetCurrentLocation();
         }
-        private async Task<Geolocation> GetCachedLocation()
+        public async Task<BlazoredGeolocation> GetCurrentLocation()
         {
-            Geolocation geolocation = null;
+            BlazoredGeolocation geolocation = null;
             try
             {
-                Location location = await Microsoft.Maui.Devices.Sensors.Geolocation.Default.GetLastKnownLocationAsync();
+                _isCheckingLocation = true;
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                _cancelTokenSource = new CancellationTokenSource();
+
+                Location location = await MauiGeolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
 
                 if (location != null)
                 {
@@ -38,6 +48,69 @@ namespace BlazoredLocation.Services
                             }
                         }
                     };
+                }
+                if (geolocation != null)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                geolocation = new() { Message = "Location information is unavailable.", Code = LocationErrorsEnum.POSITION_UNAVAILABLE };
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                geolocation = new() { Message = "Location information is unavailable.", Code = LocationErrorsEnum.POSITION_UNAVAILABLE };
+            }
+            catch (PermissionException pEx)
+            {
+                geolocation = new() { Message = "User denied the request for Geolocation.", Code = LocationErrorsEnum.PERMISSION_DENIED };
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                geolocation = new() { Message = "Your App does not have permission to access location data.", Code = LocationErrorsEnum.PERMISSION_DENIED };
+            }
+            catch (Exception ex)
+            {
+                geolocation = new() { Message = "An unknown error occurred.", Code = LocationErrorsEnum.UNKNOWN_ERROR };
+            }
+            finally
+            {
+                _isCheckingLocation = false;
+            }
+            return geolocation;
+        }
+
+        public void CancelRequest()
+        {
+            if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+                _cancelTokenSource.Cancel();
+        }
+        private async Task<BlazoredGeolocation> GetCachedLocation()
+        {
+            BlazoredGeolocation geolocation = null;
+            try
+            {
+                Location location = await MauiGeolocation.Default.GetLastKnownLocationAsync();
+
+                if (location != null)
+                {
+                    geolocation = new()
+                    {
+                        GeolocationPosition = new()
+                        {
+                            GeolocationCoordinates = new()
+                            {
+                                Altitude = location.Altitude,
+                                Latitude = location.Latitude,
+                                Longitude = location.Longitude
+                            }
+                        }
+                    };
+                }
+                else
+                {
+                    geolocation = await GetCurrentLocation();
                 }
             }
             catch (FeatureNotSupportedException fnsEx)
